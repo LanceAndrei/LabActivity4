9400ec38e5bc04b1dcc90305dc355c240d0c5d57
LanceAndrei <2015084992@ust-ics.mygbiz.com>
2018-09-29 18:41:14 +0800

    Initial commit

---
+++ app/src/main/java/lance/wico/com/wicolancelab4/MainActivity.java
@@ -0,0 +1,58 @@
+package lance.wico.com.wicolancelab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button toastBtn = (Button) findViewById(R.id.toastBtn);
+        Button snackbarBtn = (Button) findViewById(R.id.snackbarBtn);
+
+        toastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String message = "TOAST!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(getApplicationContext(),message,duration).show();
+            }
+        });
+
+        snackbarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = findViewById(R.id.main_layout_id);
+                String message = "Snackbar message";
+                int duration = Snackbar.LENGTH_SHORT;
+
+                showSnackbar(view, message, duration);
+
+            }
+        });
+
+
+    }
+
+    public void showSnackbar(View view, String message, int duration)
+    {
+        Snackbar.make(view, message, duration).show();
+    }
+
+}
+
+
+
+
+
